61b9f3eb1b8fcc48f17d85f7a73152f8afee7bd0
rob <rob@codebox.org.uk>
2015-09-08 11:27:11 +0100

    added directed adjacency list

--- graph_data_structures.py
+++ graph_data_structures.py
@@ -17,3 +17,31 @@ class DirectedAdjacencyMatrix:
 
     def get_incoming(self, i_to):
         return [i for i in range(self.n) if self.data[i][i_to]]
+
+class DirectedAdjacencyList:
+    def __init__(self, n):
+        self.n = n
+        self.vertices = range(n)
+        self.edges = []
+
+    def connect(self, i_from, i_to):
+        self.edges.append([i_from, i_to])
+
+    def disconnect(self, i_from, i_to):
+        for edge in self.edges:
+            if edge[0] == i_from and edge[1] == i_to:
+                self.edges.remove(edge)
+                break
+
+    def are_connected(self, n1, n2):
+        for edge in self.edges:
+            if (edge[0] == n1 and edge[1] == n2) or (edge[0] == n2 and edge[1] == n1):
+                return True
+
+        return False
+
+    def get_outgoing(self, i_from):
+        return list(set([edge[1] for edge in self.edges if edge[0] == i_from]))
+
+    def get_incoming(self, i_to):
+        return list(set([edge[0] for edge in self.edges if edge[1] == i_to]))
--- test_graph_data_structures.py
+++ test_graph_data_structures.py
@@ -1,73 +1,76 @@
 import unittest
-from graph_data_structures import DirectedAdjacencyMatrix
+from graph_data_structures import DirectedAdjacencyMatrix, DirectedAdjacencyList
 
-class TestDirectedAdjacencyMatrix(unittest.TestCase):
+class TestStructure(unittest.TestCase):
 
     def test_are_connected(self):
-        m = DirectedAdjacencyMatrix(5)
-
-        self.assertEqual(m.are_connected(1,2), False)
-        self.assertEqual(m.are_connected(2,1), False)
-        m.connect(1,2)
-        self.assertEqual(m.are_connected(1,2), True)
-        self.assertEqual(m.are_connected(2,1), True)
-        m.connect(1,2)
-        self.assertEqual(m.are_connected(1,2), True)
-        self.assertEqual(m.are_connected(2,1), True)
+        self.assertEqual(self.m.are_connected(1,2), False)
+        self.assertEqual(self.m.are_connected(2,1), False)
+        self.m.connect(1,2)
+        self.assertEqual(self.m.are_connected(1,2), True)
+        self.assertEqual(self.m.are_connected(2,1), True)
+        self.m.connect(1,2)
+        self.assertEqual(self.m.are_connected(1,2), True)
+        self.assertEqual(self.m.are_connected(2,1), True)
 
     def test_get_incoming(self):
-        m = DirectedAdjacencyMatrix(5)
-
-        self.assertEqual(m.get_incoming(1), [])        
-        m.connect(1,2)
-        self.assertEqual(m.get_incoming(1), [])
-        self.assertEqual(m.get_incoming(2), [1])
-        m.connect(3,2)
-        self.assertEqual(m.get_incoming(2), [1,3])
-        m.connect(3,2)
-        self.assertEqual(m.get_incoming(2), [1,3])
+        self.assertEqual(self.m.get_incoming(1), [])        
+        self.m.connect(1,2)
+        self.assertEqual(self.m.get_incoming(1), [])
+        self.assertEqual(self.m.get_incoming(2), [1])
+        self.m.connect(3,2)
+        self.assertEqual(self.m.get_incoming(2), [1,3])
+        self.m.connect(3,2)
+        self.assertEqual(self.m.get_incoming(2), [1,3])
 
     def test_get_outgoing(self):
-        m = DirectedAdjacencyMatrix(5)
-
-        self.assertEqual(m.get_outgoing(1), [])  
-        m.connect(1,2)
-        self.assertEqual(m.get_outgoing(1), [2])
-        self.assertEqual(m.get_outgoing(2), [])
-        m.connect(1,3)
-        self.assertEqual(m.get_outgoing(1), [2,3])
-        m.connect(1,3)
-        self.assertEqual(m.get_outgoing(1), [2,3])
+        self.assertEqual(self.m.get_outgoing(1), [])  
+        self.m.connect(1,2)
+        self.assertEqual(self.m.get_outgoing(1), [2])
+        self.assertEqual(self.m.get_outgoing(2), [])
+        self.m.connect(1,3)
+        self.assertEqual(self.m.get_outgoing(1), [2,3])
+        self.m.connect(1,3)
+        self.assertEqual(self.m.get_outgoing(1), [2,3])
         
     def test_connect_disconnect(self):
         def are_connected(b):
-            self.assertEqual(m.are_connected(1,2), b)
-            self.assertEqual(m.are_connected(2,1), b)
-
-        m = DirectedAdjacencyMatrix(5)
+            self.assertEqual(self.m.are_connected(1,2), b)
+            self.assertEqual(self.m.are_connected(2,1), b)
 
         are_connected(False)
 
-        m.connect(1,2) # 1 connection
+        self.m.connect(1,2) # 1 connection
         are_connected(True)
         
-        m.disconnect(1,2) # 0 connections
+        self.m.disconnect(1,2) # 0 connections
         are_connected(False)
         
-        m.disconnect(1,2) # 0 connections
+        self.m.disconnect(1,2) # 0 connections
         are_connected(False)
         
-        m.connect(1,2) # 1 connection
+        self.m.connect(1,2) # 1 connection
         are_connected(True)
 
-        m.connect(1,2) # 2 connections
+        self.m.connect(1,2) # 2 connections
         are_connected(True)
 
-        m.disconnect(1,2) # 1 connection
+        self.m.disconnect(1,2) # 1 connection
         are_connected(True)
 
-        m.disconnect(1,2) # 0 connections
+        self.m.disconnect(1,2) # 0 connections
         are_connected(False)
+
+class TestDirectedAdjacencyMatrix(TestStructure):
+    def setUp(self):
+        self.m = DirectedAdjacencyMatrix(5)
+
+class TestDirectedAdjacencyList(TestStructure):
+    def setUp(self):
+        self.m = DirectedAdjacencyList(5)
 
 if __name__ == '__main__':
-    unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestDirectedAdjacencyMatrix)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestDirectedAdjacencyList)
+    unittest.TextTestRunner(verbosity=2).run(suite)
